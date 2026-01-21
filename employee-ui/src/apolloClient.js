5375593effe9a4e7c08efd97300b5a21bc4fb351
johndonga72 <johndonga72@gmail.com>
2026-01-21 12:14:54 +0530

    deploy

--- employee-ui/src/apolloClient.js
+++ employee-ui/src/apolloClient.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
-    uri: "http://127.0.0.1:8000/graphql/",
+    uri: "https://graphql-lzcc.onrender.com",
     cache: new InMemoryCache(),
 });
 export default client;
